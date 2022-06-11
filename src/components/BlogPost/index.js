ac42b738f7870e19ca87fe1f410ebdac42ffb6ec
piratejas <piratejas@gmail.com>
2022-06-11 13:54:11 +0100

    added figcaption and updated plan

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -4,13 +4,16 @@ function BlogPost({title, author, datePosted, content, imageSrc, imageAlt}) {
       <h1 className="title">{title}</h1>
       <h2 className="author">by {author}</h2>
       <h4 className="date">{datePosted}</h4>
-      <img 
-      className="image"
-      width= "100%"
-      height= "auto" 
-      src={imageSrc} 
-      alt={imageAlt} 
-      />
+      <figure>
+        <img 
+        className="image"
+        width= "100%"
+        height= "auto" 
+        src={imageSrc} 
+        alt={imageAlt} 
+        />
+        <figcaption>{imageAlt}</figcaption>
+      </figure>
       <p>{content}</p>
     </div>
   );
