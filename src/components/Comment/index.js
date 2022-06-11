fbb18cef011eb40e99815efa5cd9cd6fa441a0c3
piratejas <piratejas@gmail.com>
2022-06-11 16:05:41 +0100

    updated plan for CommentList component & created comments.js data file

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,7 +1,7 @@
 function Comment({author, content, initials}) {
   return (
     <div className="comment-container">
-      <p className="initial-circle">{initials}</p>
+      <span className="initial-circle">{initials}</span>
       <p className="comment">{content}</p>
       <p className="comment-author">{author}</p>
     </div>
---
+++ src/data/comments.js
@@ -0,0 +1,14 @@
+const commentsList = [
+    {
+      id: "kskBC5HZ8qgNQUiW6If6q",
+      author: "Billy Bootcamper",
+      content: "Hello, great post",
+    },
+    {
+      id: "jFyGAKz1VsGputO1gV8xa",
+      author: "Chris Meah",
+      content: "Many thank yous",
+    },
+  ];
+
+export default commentsList;
