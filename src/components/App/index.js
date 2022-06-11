7d6c4e7b4d8b2dea7685433afc65f922879acea3
piratejas <piratejas@gmail.com>
2022-06-11 14:50:39 +0100

    task 2: added Comment component, displaying comment, author & initials but unstyled

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,25 @@
 import BlogPost from '../BlogPost';
 import { blog } from '../../data/blogs.js';
+import Comment from '../Comment';
 
 function App() {
+
+  function getInitials(author) {
+    let initials = author.match(/(\b\S)?/g).join("").toUpperCase();
+    return initials
+  }
+
+  const commentProps = {
+    author: "Ben Jerry Lee",
+    content: "Hello, great post",
+    initials: "",
+  }
 
   return (
     <div className= "App">
       <BlogPost {...blog}/>
+      <Comment 
+      {...commentProps} initials= {getInitials(commentProps.author)}/>
     </div>
   )
 
--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,5 +1,11 @@
-function Comment() {
-  return null;
+function Comment({author, content, initials}) {
+  return (
+    <div className="comment-container">
+      <p className="initial-circle">{initials}</p>
+      <p className="comment">{content}</p>
+      <p className="comment-author">{author}</p>
+    </div>
+  );
 }
 
 export default Comment;
